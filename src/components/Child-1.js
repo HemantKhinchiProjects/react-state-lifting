c3094594a0c001ab8a797e98cc7db4886ce6f403
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-06-15 16:26:10 +0530

    1 file modified

--- src/components/Child-1.js
+++ src/components/Child-1.js
@@ -1,6 +1,6 @@
 import React from 'react';
 const ChildOne = (props) => {
-  const name = ['Hemant', 'khinchi'];
+  const name = ['Hemant', 'Khinchi'];
   return (
     <div>
       <p>Child 1 <span style={{color:'red'}}>{props.fname(name)}</span></p>
